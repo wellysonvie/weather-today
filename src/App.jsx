487e367bdd7143741fa83278941f603299d7a440
wellysonvie <wellysonvie@gmail.com>
2021-05-13 15:15:06 -0300

    feat: trocar background de acordo com clima e horário

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
+import { useContext } from 'react';
 import CitySelector from './components/CitySelector';
 import CityWeather from './components/CityWeather';
-import { CityWeatherProvider } from './contexts/CityWeatherContext';
+import { CityWeatherContext } from './contexts/CityWeatherContext';
 
 import styles from './styles/app.module.scss';
 
 function App() {
 
+  const { isNight, weatherStatus } = useContext(CityWeatherContext);
+
+  const backgrounds = {
+    'clearSky': isNight ? 'clearSky-night.jpg' : 'clearSky-morning.jpg',
+    'cloud': isNight ? 'fewClouds-night.jpg' : 'fewClouds-morning.jpg',
+    'clouds': isNight ? 'cloudy-night.jpg' : 'cloudy-morning.jpg',
+    'rain': isNight ? 'rain-night.jpg' : 'rain-morning.jpg',
+    'default': isNight ? 'clearSky-night.jpg' : 'clearSky-morning.jpg',
+  }
+
+  console.log(backgrounds[weatherStatus]);
+
   return (
-    <CityWeatherProvider>
-      <div className={styles.containerApp}>
-        <div
-          className={styles.background}
-          style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/backgrounds/clearSky.jpg')` }}
-        ></div>
-        <CityWeather />
-        <CitySelector />
-      </div>
-    </CityWeatherProvider>
+    <div className={styles.containerApp}>
+      <div
+        className={styles.background}
+        style={{ backgroundImage: `url('${process.env.PUBLIC_URL}/backgrounds/${backgrounds[weatherStatus]}')` }}
+      ></div>
+      <CityWeather />
+      <CitySelector />
+    </div>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import { CityWeatherProvider } from './contexts/CityWeatherContext';
 
 ReactDOM.render(
-  <App />,
+  <CityWeatherProvider>
+    <App />
+  </CityWeatherProvider>,
   document.getElementById('root')
 );
